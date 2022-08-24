c68c01bfc8510b428c4375c7cf33601f33c886bc
Phillip <90676461+ParteeHat@users.noreply.github.com>
2022-08-24 05:34:29 +0000

    Add files via upload

---
+++ 1twoSum.js
@@ -0,0 +1,16 @@
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i == j) {
+        continue;
+      }
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+  return 'hiu';
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(1);
